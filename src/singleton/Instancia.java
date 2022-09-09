0f8ca9cfeeec030f6316de5600ecaf488bdd4d9f
604PROG10-64769\Internet <elkinm.salcedo212@umariana.edu.co>
2022-09-09 08:13:13 -0500

    singleton

---
+++ src/singleton/Instancia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singleton;
+
+/**
+ * @version 1.0
+ * @author Internet
+ * @class Instancia
+ * Clase de instancia usada para el patron singleton
+ */
+public class Instancia {
+    
+    private Instancia instancia;
+}
